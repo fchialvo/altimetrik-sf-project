ab94b3cd36fd3e8f4aef662c21247306eeef4652
fchialvo <francochialvo03@gmail.com>
2023-12-03 05:13:50 -0300

    create lwc and lightning app

---
+++ force-app/main/default/lwc/invokeService/invokeService.js
@@ -0,0 +1,25 @@
+import { LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import processData from "@salesforce/apex/ucl_NightlyService.processData";
+
+export default class InvokeService extends LightningElement {  
+  handleToggleClick() {
+    processData()
+      .then(() => {
+        const event = new ShowToastEvent({
+          title: "Success",
+          message: "Data processed successfully",
+          variant: "success",
+        });
+        this.dispatchEvent(event);
+      })
+      .catch((error) => {
+        const event = new ShowToastEvent({
+          title: "Error",
+          message: error.body.message,
+          variant: "error",
+        });
+        this.dispatchEvent(event);
+      });
+  }
+}
